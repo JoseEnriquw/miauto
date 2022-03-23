3b0972a83392f5a167277e359294d6af2b2969b6
JoseEnriquw <63372900+JoseEnriquw@users.noreply.github.com>
2022-03-23 20:36:23 -0300

    Se armo preproduccion

--- LogginApi/miauto.identity.api/Models/Request/AuthRequest.cs
+++ LogginApi/miauto.identity.api/Models/Request/AuthRequest.cs
@@ -5,7 +5,7 @@ namespace miauto.identity.api.Models.Request
     public class AuthRequest
     {
         [Required]
-        public string email { get; set; }
+        public string email { get; set;  }
         [Required]
         public string password { get; set; }
     }
